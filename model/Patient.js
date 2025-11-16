d33a7cbbe9703f7226b9890da4eb3286dcd1e10a
Jasmin fathima <jasminpeer006@gmail.com>
2025-11-16 18:35:13 +0530

    Update Patient.js

--- model/Patient.js
+++ model/Patient.js
@@ -3,14 +3,17 @@ import mongoose from "mongoose";
 
 const patientSchema = new mongoose.Schema(
   {
-    name: { type: String, required: true },
-    age: { type: Number },
-    bloodGroup: { type: String, required: true },
+    name: { type: String, required: true, trim: true },
+    age: { type: Number, required: true },
+    bloodGroup: {
+      type: String,
+      required: true,
+      enum: ["A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"],
+    },
     units: { type: Number, required: true, default: 1 },
     emergency: { type: Boolean, default: false },
   },
   { timestamps: true }
 );
 
-const Patient = mongoose.model("Patient", patientSchema);
-export default Patient; // use export default
+export default mongoose.model("Patient", patientSchema);
